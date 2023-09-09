5e419beb969ec953af2f3f9c32abdfcc7b566bb4
rony1980 <rauf.rony@gmail.com>
2023-09-09 19:53:13 +0600

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+
+export const metadata = {
+  title: 'Ronys Threads',
+  description: 'Hello Threads',
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/form/Comment.tsx
@@ -0,0 +1,89 @@
+'use client';
+import * as z from 'zod'; // for validation
+import { useForm } from 'react-hook-form';
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '../ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { CommentValidation } from '@/lib/validations/thread';
+import Image from 'next/image';
+import { addCommentToThread } from '@/lib/actions/thread.action';
+//import { createThread } from '@/lib/actions/thread.action';
+// import { updateUser } from '@/lib/actions/user.action';
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+          <FormField
+            control={form.control}
+            name="thread"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-3 w-full">
+                <FormLabel>
+                  <Image
+                    src={currentUserImg}
+                    alt="profile Image"
+                    width={48}
+                    height={48}
+                    className="rounded-full object-cover"
+                  />
+                </FormLabel>
+                <FormControl className="border-none bg-transparent">
+                  <Input
+                    type="text"
+                    placeholder="Comment..."
+                    className="no-focus text-light-1 outline-none"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="comment-form_btn">
+            Replay
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default Comment;
